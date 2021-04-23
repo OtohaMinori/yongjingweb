638a3970a1449d7a23056c9d6d275f7c45f29127
SJ Bots\結城あやの <YuukiAyano@SJBots.com>
2021-04-23 19:56:09 +0900

    JavaScript Test

--- loginsystem.php
+++ loginsystem.php
@@ -3,7 +3,7 @@ $Account = $_POST['Account'];
 $Password = $_POST['Password'];
 
 if ($Account == "YuukiAyano" && $Password == "Ayano1227") {
-    echo "<script src=\"./js/logedin.js\"></script>";
+    echo "<script src=js/logedin.js></script>";
 } else {
-    echo "<script src=\"./js/accessdenied.js\"></script>";
+    echo "<script src=./js/accessdenied.js></script>";
 }
